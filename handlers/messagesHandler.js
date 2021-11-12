c0f3786a1e44e054a04d84183ec6eddf631f31c4
Thiago Marback <tmarback@sympho.dev>
2021-11-12 00:42:56 -0800

    Return error on not implemented methods.

--- handlers/messagesHandler.js
+++ handlers/messagesHandler.js
@@ -3,21 +3,27 @@
 module.exports = {
   getMessages: (req, res) => {
     
+    throw { status: 501, message: "Not implemented yet" };
+    /*
     res.status(200).json({
       page: 1,
       pageCount: 1,
       pages: []
     });
+    */
 
   },
 
   postMessage: (req, res) => {
 
+    throw { status: 501, message: "Not implemented yet" };
+    /*
     const timestamp = new Date().toISOString();
     res.status(201).json({
       id: 0,
       timestamp: timestamp,
     });
+    */
 
   }, 
 };
