2a8f6f33b566b3dcc3d541cac2cacf9b1074ed7a
Thiago Marback <tmarback@sympho.dev>
2021-11-20 07:38:43 -0800

    Added definition of SQL database.

---
+++ sql/data.sql
@@ -0,0 +1,15 @@
+CREATE SCHEMA IF NOT EXISTS anniv3;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE IF NOT EXISTS anniv3.users(
+    id          UUID            PRIMARY KEY DEFAULT uuid_generate_v4(),
+    username    VARCHAR( 100 )  NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS anniv3.messages(
+    id          SERIAL                      PRIMARY KEY,
+    author      UUID                        REFERENCES anniv3.users ( id ) ON UPDATE CASCADE ON DELETE CASCADE,
+    content     VARCHAR( 4096 )             NOT NULL,
+    time_posted TIMESTAMP WITH TIME ZONE    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE ( author ) -- We're only allowing one message per person
+);
