049433df63b7d407922916aab3865ab430f0a3f8
Thiago Marback <tmarback@sympho.dev>
2021-11-20 19:18:32 -0800

    Release client at the end of message GET.

--- server/handlers/messagesHandler.js
+++ server/handlers/messagesHandler.js
@@ -3,46 +3,51 @@
 const { getClient } = require( './database' );
 
 module.exports = {
-  getMessages: (req, res) => {
+  getMessages: async (req, res) => {
 
     const page = req.query.page;
     const pageSize = req.query.pageSize;
 
-    getClient().then( client => {
-            return Promise.all([
-                client.query( `
-                    SELECT   anniv3.messages.id AS id, username AS author, content, time_posted AS timestamp
-                    FROM     anniv3.messages INNER JOIN anniv3.users ON anniv3.messages.author = anniv3.users.id
-                    ORDER BY time_posted ASC, id ASC
-                    LIMIT    $1
-                    OFFSET   $2
-                `, [ pageSize, ( page - 1 ) * pageSize ] ),
-                client.query(`
-                    SELECT COUNT(*) AS total
-                    FROM   anniv3.messages
-                ` ) ]);
-        }).then( data => {
-            const pageData = data[0].rows;
-            const pageCount = Math.ceil( data[1].rows[0].total / pageSize );
-            if ( pageData.length > 0 ) {
-                res.status( 200 ).json({
-                    page: page,
-                    pageSize: pageSize,
-                    pageCount: pageCount,
-                    pages: pageData,
-                });
-            } else {
-                res.status( 404 ).json({
-                    status: 404,
-                    message: "Page does not exist",
-                    pageCount: pageCount,
-                });
-            }
-        });
+    const client = await getClient();
+    Promise.all([
+        client.query( `
+            SELECT   anniv3.messages.id AS id, username AS author, content, time_posted AS timestamp
+            FROM     anniv3.messages INNER JOIN anniv3.users ON anniv3.messages.author = anniv3.users.id
+            ORDER BY time_posted ASC, id ASC
+            LIMIT    $1
+            OFFSET   $2
+        `, [ pageSize, ( page - 1 ) * pageSize ] ),
+        client.query(`
+            SELECT COUNT(*) AS total
+            FROM   anniv3.messages
+        ` )
+    ]).then( data => {
+        const pageData = data[0].rows;
+        const pageCount = Math.ceil( data[1].rows[0].total / pageSize );
+        if ( pageData.length > 0 ) {
+            res.status( 200 ).json({
+                page: page,
+                pageSize: pageSize,
+                pageCount: pageCount,
+                pages: pageData,
+            });
+        } else {
+            res.status( 404 ).json({
+                status: 404,
+                message: "Page does not exist",
+                pageCount: pageCount,
+            });
+        }
+    }).finally(() => {
+        client.release();
+    });
 
   },
 
   postMessage: (req, res) => {
+
+    const author = req.body.author;
+    const content = req.body.content;
 
     throw { status: 501, message: "Not implemented yet" };
     /*
